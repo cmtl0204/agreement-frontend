438dc197d5682be2f5f5a18cbb92f4f6aa4ec4a7
Bl4dimir98 <bladimiralex@hotmail.com>
2024-07-16 21:17:43 -0500

    fix-bugs-view-module

--- src/app/pages/core/agreement/view/view.module.ts
+++ src/app/pages/core/agreement/view/view.module.ts
@@ -1,39 +1,38 @@
-import {NgModule} from '@angular/core';
-import {CommonModule, NgOptimizedImage} from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
 
-import {ViewRoutingModule} from './view-routing.module';
-import {ViewComponent} from './view.component';
-import {ReactiveFormsModule, FormsModule} from '@angular/forms';
-import {AvatarModule} from 'primeng/avatar';
-import {ButtonModule} from 'primeng/button';
-import {CalendarModule} from 'primeng/calendar';
-import {CardModule} from 'primeng/card';
-import {CheckboxModule} from 'primeng/checkbox';
-import {DividerModule} from 'primeng/divider';
-import {DropdownModule} from 'primeng/dropdown';
-import {FileUploadModule} from 'primeng/fileupload';
-import {InputNumberModule} from 'primeng/inputnumber';
-import {InputTextModule} from 'primeng/inputtext';
-import {InputTextareaModule} from 'primeng/inputtextarea';
-import {MessageModule} from 'primeng/message';
-import {OverlayPanelModule} from 'primeng/overlaypanel';
-import {PanelModule} from 'primeng/panel';
-import {PasswordModule} from 'primeng/password';
-import {RadioButtonModule} from 'primeng/radiobutton';
-import {RippleModule} from 'primeng/ripple';
-import {StepperModule} from 'primeng/stepper';
-import {TableModule} from 'primeng/table';
-import {ToggleButtonModule} from 'primeng/togglebutton';
-import {ToolbarModule} from 'primeng/toolbar';
-import {BasicDataComponent} from './basic-data/basic-data.component';
-import {SharedModule} from '@shared/shared.module';
+import { ViewRoutingModule } from './view-routing.module';
+import { ViewComponent } from './view.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { AvatarModule } from 'primeng/avatar';
+import { ButtonModule } from 'primeng/button';
+import { CalendarModule } from 'primeng/calendar';
+import { CardModule } from 'primeng/card';
+import { CheckboxModule } from 'primeng/checkbox';
+import { DividerModule } from 'primeng/divider';
+import { DropdownModule } from 'primeng/dropdown';
+import { FileUploadModule } from 'primeng/fileupload';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { MessageModule } from 'primeng/message';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
+import { PanelModule } from 'primeng/panel';
+import { PasswordModule } from 'primeng/password';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { RippleModule } from 'primeng/ripple';
+import { StepperModule } from 'primeng/stepper';
+import { TableModule } from 'primeng/table';
+import { ToggleButtonModule } from 'primeng/togglebutton';
+import { ToolbarModule } from 'primeng/toolbar';
+import { BasicDataComponent } from './basic-data/basic-data.component';
+import { SharedModule } from '@shared/shared.module';
 import { AddendumComponent } from './addendum/addendum.component';
 import { AgreementAdministratorComponent } from './agreement-administrator/agreement-administrator.component';
 import { AppearerComponent } from './appearer/appearer.component';
-import {AgreementDateComponent} from "./agreement-date/agreement-date.component";
+import { AgreementDateComponent } from "./agreement-date/agreement-date.component";
 import { FinancingComponent } from './financing/financing.component';
 import { ObligationComponent } from './obligation/obligation.component';
-import { RegistrationRoutingModule } from 'src/app/pages/auth/registration/registration-routing.module';
 import { TagModule } from 'primeng/tag';
 import { ToastModule } from 'primeng/toast';
 
@@ -51,7 +50,6 @@ import { ToastModule } from 'primeng/toast';
   ],
   imports: [
     CommonModule,
-    RegistrationRoutingModule,
     ReactiveFormsModule,
     SharedModule,
     ButtonModule,
