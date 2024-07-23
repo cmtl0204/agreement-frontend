c127e8cf56a5f3e9ee6190531153cd8e2a2620b0
RobertoHuilco <rag.huilco@yavirac.edu.ed>
2024-07-23 16:14:37 -0500

    Update

--- src/app/pages/core/agreement/view/addendum/addendum.component.ts
+++ src/app/pages/core/agreement/view/addendum/addendum.component.ts
@@ -1,43 +1,29 @@
 import { Component, inject, Input, OnInit } from '@angular/core';
-import { CoreService, MessageDialogService, RoutesService } from '@servicesApp/core';
-import { CataloguesHttpService } from '@servicesHttp/core';
-import { AddendumEnum, SkeletonEnum } from '@shared/enums';
-import { PrimeIcons,MessageService } from 'primeng/api';
+import { CoreService} from '@servicesApp/core';
+import { AddendumEnum} from '@shared/enums';
 import { AddendumModel } from '@models/core/addendum.model';
-import { CatalogueModel, ColumnModel } from '@models/core';
-
+import { AgreementModel, ColumnModel } from '@models/core';
 
 @Component({
   selector: 'app-addendum',
   templateUrl: './addendum.component.html',
   styleUrl: './addendum.component.scss'
 })
-export class AddendumComponent implements OnInit {
+export class AddendumComponent{
 
   /** Services **/
-  protected readonly cataloguesHttpService = inject(CataloguesHttpService);
   protected readonly coreService = inject(CoreService);
-  private readonly routesService = inject(RoutesService);
-  public readonly messageDialogService = inject(MessageDialogService);
 
 
   /** Form **/
-  protected columns: ColumnModel[] = [];
+  @Input({required: true}) agreement!: AgreementModel;
   protected addendumColumns: ColumnModel[]=[];
 
-  /** Foreign Keys **/
-  protected addendumPersonTypes: CatalogueModel[]=[];
-  protected positions: CatalogueModel[] = [];
-
-  /** Enums **/
-  protected readonly AddendumEnum = AddendumEnum;
-  protected readonly SkeletonEnum = SkeletonEnum;
-  protected readonly PrimeIcons = PrimeIcons;
-  // constructor
-  constructor(private messageService: MessageService) {
+  /** build Columns**/
+  constructor() {
     this.buildaddendumColumns();
-
   }
+
   buildaddendumColumns() {
     this.addendumColumns = [
       {
@@ -56,38 +42,5 @@ export class AddendumComponent implements OnInit {
         field: 'agreementEndedAt', header: AddendumEnum.agreementEndedAt
       },
     ];
-  }
-
-  addendums: AddendumModel[] = [
-    {
-      id: "1",
-      agreementId: "1",
-      isAddendum: true,
-      description: "Descripcion",
-      isModifiedFinishDate: true,
-      agreementEndedAt: new Date("2019-01-04"),
-    }
-  ];
-
-  ngOnInit(): void {
-
-  }
-
-  findAgrement() {
-
-  }
-
-  /** Redirects **/
-  redirectRegistration() {
-    // this.messageDialogService.questionOnExit().subscribe(result => {
-    //   if (result) {
-    //     this.onLeave = true;
-    //     this.routesService.registration();
-    //   } else {
-    //     this.onLeave = false;
-    //   }
-    // });
-
-    this.routesService.registration();
   }
 }
