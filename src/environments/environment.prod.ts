dabb9ad1e059181ce0ba2adfb4af451a7f8981c9
Mauricio Tamayo <cesar.tamayo@turismo.gob.ec>
2024-08-22 10:01:29 -0500

    change ip

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: 'http://192.168.20.143:8082',
+  API_URL: 'http://192.168.20.42:8082',
   APP_NAME:'Pruebas Sistema Informático de Convenios',
   APP_SHORT_NAME:'SIC'
 };
