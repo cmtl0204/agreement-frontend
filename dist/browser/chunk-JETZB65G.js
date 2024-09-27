ca1ca2eba1e50fdb44fa560d7f4312db7ae384c7
cmtl0204 <cesar.tamayo0204@gmail.com>
2024-09-26 19:35:05 -0500

    fix bus select role

--- dist/browser/chunk-JETZB65G.js
+++
@@ -1,1 +0,0 @@
-import{a}from"./chunk-FQUNBJ7E.js";import"./chunk-I6ACFXRE.js";import"./chunk-RL7JZA26.js";import"./chunk-XHLRH3DP.js";import"./chunk-EXGEWYYG.js";import"./chunk-EUOB27Q6.js";export{a as AuthModule};
---
+++ dist/browser/chunk-NIBQ7NCV.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-6ELDOP6K.js";import"./chunk-I6ACFXRE.js";import"./chunk-RL7JZA26.js";import"./chunk-XHLRH3DP.js";import"./chunk-EXGEWYYG.js";import"./chunk-EUOB27Q6.js";export{a as AuthModule};
