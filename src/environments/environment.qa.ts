780b09db98de099482bae7f9e768f3db43295abc
cmtl0204 <cesar.tamayo0204@gmail.com>
2026-09-10 15:49:10 -0500

    change api

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  API_URL: 'https://api-convenios-des.turismo.gob.ec',
+  API_URL: 'https://api-sic.economicoproductivo.gob.ec',
   APP_NAME:'Pruebas QA Sistema Informático de Convenios',
   APP_SHORT_NAME:'SIC'
 };
